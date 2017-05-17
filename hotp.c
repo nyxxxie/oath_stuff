70bb0b956695205e83247ef84588a9bd08b65e97
Nyxxie <nyxxxxie@gmail.com>
2017-05-17 00:54:19 -0500

    Added some body code and a link to the spec.

--- hotp.c
+++ hotp.c
@@ -12,4 +12,28 @@ this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
 Street, Fifth Floor, Boston, MA  02110-1301, USA.
 \*****************************************************************************/
 
+#include "hotp.h"
+#include "hmac_sha1.h"
 
+struct hotp_digits {
+
+};
+
+// https://www.ietf.org/rfc/rfc4226.txt
+
+int hotp(int number_digits
+         const uint8_t* key, size_t key_len,
+         const uint8_t* text, size_t text_len,
+         hotp_digits* hotp)
+{
+    int rc;
+    uint8_t hmac[20];
+
+    /* Calc HMAC */
+    rc = hmac_sha1(key, key_len, text, text_len, hmac);
+    if (rc < 0) {
+        return rc;
+    }
+
+    return 0;
+}
